9a41943b6e7fc2e1834bf481f0db5bb805f3cc55
johnweber <john.weber@basilicom.de>
2017-12-18 14:53:24 +0100

    new Idea for entries

--- src/reducers/calendar.js
+++ src/reducers/calendar.js
@@ -1,56 +1,58 @@
 import * as ActionTypes from 'actions/types';
 
 const defaultState = {
-  rooms: {
-    red: {
-      entries: [],
-    },
-    blue: {
-      entries: [],
-    },
-  }
+    rooms: {
+        red: {
+            entries: [],
+        },
+        blue: {
+            entries: [],
+        },
+    }
 };
 
 export default function reducer(state = defaultState, action) {
     switch (action.type) {
         case ActionTypes.GET_ENTRIES: {
             const { red, blue } = action.payload.data ? action.payload.data.calendarListEntries : defaultState.rooms;
-            const blueEntries = Object.keys(blue.entries).map((key) => (blue.entries[key]));
-            const redEntries = Object.keys(red.entries).map((key) => (red.entries[key]));
+            const blueEntries = Object.keys(blue.entries).map((key) => (blue.entries[key]))[0];
+            const redEntries = Object.keys(red.entries).map((key) => (red.entries[key]))[0];
             return {
                 ...state,
                 rooms: {
-                  red: {
-                    entries: [
-                        ...state.rooms.red.entries,
-                        redEntries,
-                    ],
-                  },
-                  blue: {
-                    entries: [
-                        ...state.rooms.blue.entries,
-                        blueEntries,
-                    ],
-                  },
+                    red: {
+                        entries: [
+                            ...state.rooms.red.entries,
+                            redEntries,
+                        ],
+                    },
+                    blue: {
+                        entries: [
+                            ...state.rooms.blue.entries,
+                            blueEntries,
+                        ],
+                    },
                 }
             }
         }
 
         case ActionTypes.SET_CANCELLED_STATE: {
-            const {room, item} = action.payload;
+            const { room, item } = action.payload;
             const index = state.room[room].entries.indexOf(item);
-
+            const { rooms, rooms: {[room]: { entries } } } = state;
+            const newItem = {
+                ...entries[index],
+                isCancelled: !entries[index],
+            };
             return {
                 ...state,
                 rooms: {
-                    ...state.rooms,
+                    ...rooms,
                     [room]: {
                         entries: [
-                            [index]: {
-                            ...state.rooms.entries[index],
-                    isCancelled: !state.rooms.entries[index],
-            },
-                        ]
+                            ...entries.slice(index),
+                            newItem,
+                        ],
                     }
                 }
             }
